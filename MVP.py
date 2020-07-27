f80160deb5c39fb2c1ec80c436b593b65d37fd09
Alex2653 <acastillo@aoiths.org>
2020-07-27 16:06:02 -0400

    first commit

---
+++ MVP.py
@@ -0,0 +1,10 @@
+name = input(" Welcome! What is your name? ")
+Fitness_Plan = {
+    "A" : "Muscle Building",
+    "B" : "Stamina Building",
+    "C" : "Fat Burning"
+} 
+goal = input(name + " What is your Fitness goal? (Type A B or C)" + str(Fitness_Plan))
+if goal == "B":  
+    print(Fitness_Plan.list = [" Push-ups, Chair dips, Squats, Side Planks"])
+
